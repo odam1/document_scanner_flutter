d0ba36f0e5adbe47ccba609df92424d6bf3ff2b0
ODAM COURAGE <odamdeveloper@gmail.com>
2021-06-06 05:13:09 +0000

    Double tap zoom

--- lib/views/document_scanner_list_view.dart
+++ lib/views/document_scanner_list_view.dart
@@ -19,15 +19,31 @@ class _DocumentScannerListViewState extends State<DocumentScannerListView> {
   PageController? pageController;
 
   ///
+  final transformationControllers = <TransformationController>[];
+
+  ///
   int currentPage = 1;
 
   /// current bottom bar item index
   int currentBottomBarItemIndex = 0;
 
+  ///
+  TapDownDetails? doubleTapDownDetail;
+
   @override
   void initState() {
     super.initState();
     pageController = PageController();
+    widget.controller.scannedDocumentList.forEach(
+      (element) => transformationControllers.add(TransformationController()),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController?.dispose();
+    transformationControllers.forEach((element) => element.dispose());
   }
 
   @override
@@ -74,6 +90,7 @@ class _DocumentScannerListViewState extends State<DocumentScannerListView> {
         onPageChanged: (page) {
           setState(() {
             currentPage = page + 1;
+            transformationControllers.forEach((e) => e.value.setIdentity());
           });
         },
         controller: pageController,
@@ -81,38 +98,60 @@ class _DocumentScannerListViewState extends State<DocumentScannerListView> {
         itemBuilder: (context, index) {
           final document = widget.controller.scannedDocumentList[index];
           final imageBytes = document.cropImage?.image ?? document.initialImage.image;
-          return InteractiveViewer(
-            clipBehavior: Clip.none,
-            scaleEnabled: true,
-            minScale: 1.0,
-            maxScale: 5.0,
-            child: Container(
-              child: Image.memory(
-                imageBytes,
-                key: ValueKey("$index"),
-                filterQuality: FilterQuality.low,
-                fit: BoxFit.contain,
-                frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
-                  if (frame == null) {
-                    return Container(
-                      width: 60,
-                      height: 60,
-                      //color: Colors.black26,
-                      padding: const EdgeInsets.all(16),
-                      child: Builder(
-                        builder: (context) {
-                          if (Platform.isIOS) {
-                            return CupertinoActivityIndicator();
-                          }
-                          return CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
-                          );
-                        },
-                      ),
-                    );
-                  }
-                  return child;
-                },
+          return GestureDetector(
+            onDoubleTapDown: (details) {
+              doubleTapDownDetail = details;
+            },
+            onDoubleTap: () {
+              print("Double tap 1");
+              setState(() {
+                final tfc = transformationControllers[index];
+                final position = doubleTapDownDetail!.localPosition;
+                if (tfc.value.isIdentity()) {
+                  print("Double tap 2");
+                  tfc.value = Matrix4.identity()
+                    ..translate(-position.dx * 4, -position.dy * 4)
+                    ..scale(5.0);
+                } else {
+                  print("Double tap 3");
+                  tfc.value.setIdentity();
+                }
+              });
+            },
+            child: InteractiveViewer(
+              clipBehavior: Clip.none,
+              scaleEnabled: true,
+              minScale: 1.0,
+              maxScale: 5.0,
+              transformationController: transformationControllers[index],
+              child: Container(
+                child: Image.memory(
+                  imageBytes,
+                  key: ValueKey("$index"),
+                  filterQuality: FilterQuality.low,
+                  fit: BoxFit.contain,
+                  frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+                    if (frame == null) {
+                      return Container(
+                        width: 60,
+                        height: 60,
+                        //color: Colors.black26,
+                        padding: const EdgeInsets.all(16),
+                        child: Builder(
+                          builder: (context) {
+                            if (Platform.isIOS) {
+                              return CupertinoActivityIndicator();
+                            }
+                            return CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                    return child;
+                  },
+                ),
               ),
             ),
           );
